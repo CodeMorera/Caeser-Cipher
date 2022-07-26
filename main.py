d576ec984d9d67741abc077205edf37fdb2710f8
Chris <80273421+CodeMorera@users.noreply.github.com>
2022-07-26 03:18:28 +0000

    reorganized the code under one function with a few if else statements

--- main.py
+++ main.py
@@ -4,25 +4,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == "encode":
-  def encrypt(new_text,shift_num):
-    encoded_word = ""
-    for letter in new_text:
+def caeser_cipher(direction,new_text,shift_num):
+   coded_word = ""
+   spacer = 26
+   for letter in new_text:
       position = alphabet.index(letter)
-      new_position = position + shift_num
-      new_letter = alphabet[new_position]
-      encoded_word += new_letter 
-    print(f"The encoded text is {encoded_word}")
-elif direction == "decode":
-  def encrypt(new_text,shift_num):
-    decoded_word = ""
-    spacer = 26
-    for letter in new_text:
-      position = alphabet.index(letter)
-      new_position = (position + spacer) - shift_num
-      new_letter = alphabet[new_position]
-      decoded_word += new_letter
-    print(f"The decoded text is {decoded_word}")  
-
-
-encrypt(new_text=text,shift_num=shift)
+      if direction == "encode":
+        new_position = position + shift_num
+        new_letter = alphabet[new_position]
+        coded_word += new_letter 
+      elif direction == "decode":
+        new_position = (position + spacer) - shift_num
+        new_letter = alphabet[new_position]
+        coded_word += new_letter
+   print(f"The coded text is {coded_word}")
+      
+caeser_cipher(direction,new_text=text,shift_num=shift)
